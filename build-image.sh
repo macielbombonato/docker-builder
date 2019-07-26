8642d9187117b9cc8b70988b2c93b97d7649440f
Maciel Escudero Bombonato <maciel.bombonato@gmail.com>
2019-07-26 15:20:22 -0300

    Update build-image.sh
    improve some options in build shell script

--- build-image.sh
+++ build-image.sh
@@ -1,8 +1,19 @@
-export REPOSITORY_NAME="macielbombonato"
-export SERVICE_NAME="docker-builder"
-export VERSION="latest"
+export DOCKER_REPO="macielbombonato"
+export IMAGE_NAME="docker-builder"
 
 export DOCKER_FILE="."
 
-echo 'Building image'
-docker build --rm -t ${REPOSITORY_NAME}/${SERVICE_NAME}:${VERSION} ${DOCKER_FILE}
+if [ -z "$1" ]; then
+    export VERSION="latest"
+else
+    export VERSION=$1
+fi
+
+echo " ==> Image data "
+echo " - Dockerfile: ${DOCKER_FILE}" 
+echo " - REPOSITORY: ${DOCKER_REPO}" 
+echo " - IMAGE:      ${IMAGE_NAME}" 
+echo " - VERSION:    ${VERSION}" 
+
+echo ' == Building image'
+docker build --rm -t ${DOCKER_REPO}/${IMAGE_NAME}:${VERSION} -f Dockerfile ${DOCKER_FILE}
